0128de67d28c86625d96f76a80403f71b65defd8
ajaaz <mdajaaz1999@gmail.com>
2019-03-17 00:21:41 +0530

    working on line charts for stats page

---
+++ js/chart.js
@@ -0,0 +1,65 @@
+var max=87,value=27;
+var red='rgba(255,135,135,0.7)';
+var green='rgba(6,183,100,0.7)'
+var blue='rgba(135,147,255,0.7)';
+var myColor;
+if(value<=(max/3))
+    myColor=red;
+else if(value>max/3 &&value<=2*max/3 )
+    myColor=blue;
+if(value>2*max/3 && value<=max)
+    myColor=green;
+else if(value>max)
+    myColor='#888';
+
+myData=[0];
+myData.pop();
+myData.push(value);
+myData.push(max);
+
+
+var ctx = document.getElementById('module2').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        // labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: status,
+            backgroundColor: myColor,
+            data: myData,
+            borderColor:myColor,
+            borderWidth: 4,
+            borderSkipped: 'top'
+        },{
+            
+            backgroundColor: '#fff  ',
+            data: [max-value],
+            borderColor:myColor,
+            borderWidth: 4,
+            borderSkipped: 'top'
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false // hides the legend
+          },
+          tooltips: {
+            enabled: true // hides the tooltip.
+          },
+          scales: {
+            xAxes: [{
+              display: false, // hides the horizontal scale
+              stacked: true // stacks the bars on the x axis
+            }],
+            yAxes: [{
+              display: false, // hides the vertical scale
+              stacked: true // stacks the bars on the y axis
+            }]
+          }
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+function viewOverview() {
+    document.querySelector('.overview').style.display = 'block';
+    document.querySelector('.stats').style.display = 'none';
+    document.querySelector('.edit').style.display = 'none';
+}
